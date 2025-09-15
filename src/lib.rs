e37bb29147014d377fcb7e3c66f32c190ccf5565
kurtulx <kurtmaestro404@gmail.com>
2025-09-16 02:05:13 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ static PROXYIP_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"^.+-\d+$").unwra
 static PROXYKV_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([A-Z]{2})").unwrap());
 
 // Base URL for GitHub raw content
-static GITHUB_BASE_URL: &str = "https://raw.githubusercontent.com/AFRcloud/SirenWeb/refs/heads/main/web";
+static GITHUB_BASE_URL: &str = "https://raw.githubusercontent.com/kurtulx/SirenWeb/refs/heads/main/web";
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
@@ -57,7 +57,7 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
         .on_async("/link", link)
         .on_async("/converter", converter)
         .on_async("/:proxyip", tunnel)
-        .on_async("/afrcloud/:proxyip", tunnel)
+        .on_async("/tes/:proxyip", tunnel)
         .run(req, env)
         .await
 }
